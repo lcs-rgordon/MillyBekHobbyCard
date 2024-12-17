422a2eecfe3734f196bc95ced76045cb1263b2a7
Russell Gordon <rgordon@lcs.on.ca>
2024-12-17 09:08:22 -0500

    Example of using a quad curve. Get control point in the middle, and a bit higher than the line.

--- MillyBekHobbyCard/Shapes/Triquetra.swift
+++ MillyBekHobbyCard/Shapes/Triquetra.swift
@@ -17,7 +17,11 @@ struct Triquetra: Shape {
         
         // Define the shape
         path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+//        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addQuadCurve(
+            to: CGPoint(x: rect.maxX, y: rect.maxY),
+            control: CGPoint(x: rect.maxX, y: rect.maxY * 0.5)
+        )
         path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
         path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
 
